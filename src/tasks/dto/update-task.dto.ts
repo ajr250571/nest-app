4daa8ba96078301f8ecd260aa058704842c25d78
ajr250571 <ajr250571@gmail.com>
2024-04-12 17:01:48 -0300

    update dto

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,13 @@
-export interface updateTaskDto {
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
+export class UpdateTaskDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
   title?: String;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(100)
   description?: string;
 }
